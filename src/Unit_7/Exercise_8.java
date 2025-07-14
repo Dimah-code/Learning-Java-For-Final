0ea063e34ec41d78a7516660138705d4db6d863a
dimah <hammyghareghani@gmail.com>
2025-07-14 11:12:45 +0330

    Update Unit_7 & Create Exercise_8, RectangleShape, BoxShape Classes

---
+++ src/Unit_7/Exercise_8.java
@@ -0,0 +1,31 @@
+package Unit_7;
+
+import javax.swing.*;
+
+public class Exercise_8 {
+    public static void main(String[] args) {
+        RectangleShape rectangle, rectRef;
+        BoxShape box, boxRef;
+        rectangle = new RectangleShape(12,4);
+        System.out.println("Rectangle: " + rectangle);
+
+        box = new BoxShape(13, 7, 4);
+        System.out.println("Box: " + box);
+
+        rectRef = box;
+        System.out.println("Box in RectRef " + rectRef);
+
+        boxRef = (BoxShape)rectRef;
+        System.out.println("Box in BoxRef " + boxRef);
+        if(rectRef instanceof BoxShape){
+            System.out.println("RectRef is an instance of boxshape");
+        }else{
+            System.out.println("Rectref isn't an instance of boxshape");
+        }
+        if(rectangle instanceof BoxShape){
+            System.out.println("rectangle is an instance of boxshape");
+        }else{
+            System.out.println("rectangle is not an instance of boxshape");
+        }
+    }
+}
