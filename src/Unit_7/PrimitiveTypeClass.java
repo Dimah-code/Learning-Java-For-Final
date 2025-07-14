065e426e17e1fa38efe6eee92709139f59cbf7d6
dimah <hammyghareghani@gmail.com>
2025-07-14 12:35:42 +0330

    Create instanceof_Using Package for learning

---
+++ src/Unit_7/PrimitiveTypeClass.java
@@ -0,0 +1,9 @@
+package Unit_7;
+
+
+public abstract class PrimitiveTypeClass {
+    //Methods
+    public abstract boolean equals(PrimitiveTypeClass right);
+    public abstract int compareTo(PrimitiveTypeClass right);
+    public abstract String toString();
+}
