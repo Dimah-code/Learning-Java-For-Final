603b515dfd1d9ee13d32b779b528572e9b9a46b9
dimah <hammyghareghani@gmail.com>
2025-07-18 12:10:09 +0330

    fix: correct variable names in Example_2_12.main

--- src/Unit_2/Examples/Example_12/Example_2_12.java
+++ src/Unit_2/Examples/Example_12/Example_2_12.java
@@ -6,12 +6,13 @@ public class Example_2_12 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a , b , c;
-        System.out.println("Enter 2 number: ");
-        a = scanner.nextInt();
-        b = scanner.nextInt();
+        int num1 , num2;
+        System.out.println("Enter first number: ");
+        num1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        num2 = scanner.nextInt();
         scanner.close();
-        System.out.println(sumSqrt(a, b));
+        System.out.println(sumSqrt(num1, num2));
     }
     public static int sumSqrt(int x , int y){
         int sum;
