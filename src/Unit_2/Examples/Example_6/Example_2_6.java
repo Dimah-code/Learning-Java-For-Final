6828f8404e0e7602746931a293c296f41bfe124b
dimah <hammyghareghani@gmail.com>
2025-07-18 11:27:46 +0330

    feat: implement main method in Example_2_6

--- src/Unit_2/Examples/Example_6/Example_2_6.java
+++ src/Unit_2/Examples/Example_6/Example_2_6.java
@@ -2,11 +2,11 @@ package Unit_2.Examples.Example_6;
 
 public class Example_2_6 {
     public static void main(String[] args) {
-        int x = 123;
-        float y = 123.456f;
-        String text = "hamidreza";
-        System.out.printf("x = %29d \n", x);
-        System.out.printf("y = %6.2f . \n", y);
-        System.out.printf("My name is. %2s .", text);
+        int x = 125, y = 1468;
+        float m = 327.348f, n = 4351.32f;
+        String myName = "Hamidreza";
+        System.out.printf("x = %5d, y = %3d\n", x, y);
+        System.out.printf("m = %7.2f, n = %6.2f\n", m, n);
+        System.out.printf("My name is %5s, I am %5d years old", myName, 20);
     }
 }
