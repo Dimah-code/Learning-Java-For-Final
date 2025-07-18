c384d10a1ce8788546875e4c77eaf445240338d3
dimah <hammyghareghani@gmail.com>
2025-07-18 11:34:28 +0330

    fix: correct variable typo in Example_2_7.main

--- src/Unit_2/Examples/Example_7/Example_2_7.java
+++ src/Unit_2/Examples/Example_7/Example_2_7.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Example_2_7 {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int x, y, perime, area;
-        System.out.println("Enter rectangle's height");
+        int x, y, perimeter, area;
+        System.out.println("Enter rectangle's height: ");
         x = scanner.nextInt();
-        System.out.println("Enter rectangle's width");
+        System.out.println("Enter rectangle's width: ");
         y = scanner.nextInt();
-        perime = (x + y) * 2;
+        perimeter = (x + y) * 2;
         area = x * y;
 
-        System.out.printf("perimetive = %d \n", perime);
-        System.out.printf("area = %d ", area);
+        System.out.printf("Perimeter = %d \n", perimeter);
+        System.out.printf("Area = %d ", area);
     }
 }
