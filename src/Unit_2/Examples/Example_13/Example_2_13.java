d7965b9a65a64c734a6a8ea8d29a89dfe0c6620d
dimah <hammyghareghani@gmail.com>
2025-07-18 12:13:29 +0330

    fix: correct variable names in Example_2_13.main

--- src/Unit_2/Examples/Example_13/Example_2_13.java
+++ src/Unit_2/Examples/Example_13/Example_2_13.java
@@ -6,17 +6,19 @@ public class Example_2_13 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a , b , c, min , max;
-        System.out.println("Enter 3 number: ");
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-        c = scanner.nextInt();
+        int num1, num2, num3, min , max;
+        System.out.println("Enter first number: ");
+        num1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        num2 = scanner.nextInt();
+        System.out.println("Enter third number: ");
+        num3 = scanner.nextInt();
         scanner.close();
-        min = findMin(a , b , c);
-        max = findMax(a, b , c);
+        min = findMin(num1, num2, num3);
+        max = findMax(num1, num2, num3);
 
         System.out.println("Minimum number = " + min);
-        System.out.println("Max number = " + max);
+        System.out.println("Maximum number = " + max);
     }
     public static int findMin(int x , int y, int z){
         int min;
@@ -28,7 +30,6 @@ public class Example_2_13 {
         int max;
         max = x > y ? x : y;
         max = z > max ? z : max;
-
         return max;
     }
 }
