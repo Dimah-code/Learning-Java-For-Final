1484589b93f8118254dec16052c4e0e80b92e33e
dimah <hammyghareghani@gmail.com>
2025-07-19 11:34:39 +0330

    feat: update main method in Example_3_3

--- src/Unit_3/Examples/Example_3/Example_3_3.java
+++ src/Unit_3/Examples/Example_3/Example_3_3.java
@@ -3,8 +3,6 @@ package Unit_3.Examples.Example_3;
 import java.util.Scanner;
 
 public class Example_3_3 {
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         for(float f = 15; f <= 18; f+= 0.5){
             System.out.println("step = " + f);
