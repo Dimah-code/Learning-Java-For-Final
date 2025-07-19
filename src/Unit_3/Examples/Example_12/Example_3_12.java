e384f9f2ddc39013aee549abfac441ecb3316f4f
dimah <hammyghareghani@gmail.com>
2025-07-19 12:11:02 +0330

    feat: update main method in Example_3_12

--- src/Unit_3/Examples/Example_12/Example_3_12.java
+++ src/Unit_3/Examples/Example_12/Example_3_12.java
@@ -6,21 +6,21 @@ public class Example_3_12 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-       int inp, oddCounter = 0, evenCounter = 0;
+       int input, oddCounter = 0, evenCounter = 0;
        while(true){
            System.out.println("Enter a numbers to find Odd/Even (0 = stop): ");
-           inp = scanner.nextInt();
-           if(inp == 0){
+           input = scanner.nextInt();
+           if(input == 0){
                break;
            }
-           if(inp % 2 == 0){
+           if(input % 2 == 0){
                 oddCounter++;
            }else{
                evenCounter++;
            }
        }
 
-        System.out.println("Odd numbers: " + oddCounter);
-        System.out.println("Even numbers: " + evenCounter);
+        System.out.printf("%d odd number you entered\n", oddCounter);
+        System.out.printf("%d even number you entered\n", evenCounter);
     }
 }
