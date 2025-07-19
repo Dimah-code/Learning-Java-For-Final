1ae48ce515686843821b5a244309869455f2a5f0
dimah <hammyghareghani@gmail.com>
2025-07-19 11:56:19 +0330

    feat: update main method in Example_3_9

--- src/Unit_3/Examples/Example_9/Example_3_9.java
+++ src/Unit_3/Examples/Example_9/Example_3_9.java
@@ -1,10 +1,6 @@
 package Unit_3.Examples.Example_9;
 
-import java.util.Scanner;
-
 public class Example_3_9 {
-    static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         for (int i = 41; i <= 120 ; i++) {
             System.out.print(i + " = " + (char)i + "\t");
