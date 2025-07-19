725ee8c7f28dbc4d6db39ed23f3b2ffb15378392
dimah <hammyghareghani@gmail.com>
2025-07-19 14:39:55 +0330

    feat: update main method in Exercise_3_6

--- src/Unit_3/Exercises/Exercise_6/Exercise_3_6.java
+++ src/Unit_3/Exercises/Exercise_6/Exercise_3_6.java
@@ -2,7 +2,7 @@ package Unit_3.Exercises.Exercise_6;
 
 public class Exercise_3_6 {
     public static void main(String[] args) {
-        int a = 1, b = 2, number = 20, temp;
+        int a = 1, b = 2, number = 89, temp;
         System.out.printf("%d, %d, ", a , b);
         while(b <= number){
             a = a + b;
