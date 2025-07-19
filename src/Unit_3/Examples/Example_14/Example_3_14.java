5e1430d53f6e3fcbef4548edf34447c831009b6d
dimah <hammyghareghani@gmail.com>
2025-07-19 12:20:14 +0330

    feat: update main method in Example_3_14

--- src/Unit_3/Examples/Example_14/Example_3_14.java
+++ src/Unit_3/Examples/Example_14/Example_3_14.java
@@ -6,15 +6,15 @@ public class Example_3_14 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int x , y;
+        int input , output;
         System.out.println("Enter x value: ");
-        x = scanner.nextInt();
-        if(x == 0){
-            y = 10;
+        input = scanner.nextInt();
+        if(input == 0){
+            output = 10;
         }
         else{
-            y = (5 * x * x) - (3 * x + 6);
+            output = (5 * input * input) - (3 * input + 6);
         }
-        System.out.println("x = " + x + " y = " + y );
+        System.out.println("x = " + input + " output = " + output);
     }
 }
