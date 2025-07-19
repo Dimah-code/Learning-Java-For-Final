aa4ca9f8bbcc3705e89e18a32d7a1f9c106798aa
dimah <hammyghareghani@gmail.com>
2025-07-19 11:54:33 +0330

    feat: update main method in Example_3_8

--- src/Unit_3/Examples/Example_8/Example_3_8.java
+++ src/Unit_3/Examples/Example_8/Example_3_8.java
@@ -6,25 +6,25 @@ public class Example_3_8 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int fee, hours, salary;
-        System.out.println("Enter fee: ");
-        fee = scanner.nextInt();
-        System.out.println("Enter hours: ");
-        hours = scanner.nextInt();
-        calculateSalary(fee, hours);
+        int rate, hoursWorked, salary;
+        System.out.println("Enter rate: ");
+        rate = scanner.nextInt();
+        System.out.println("Enter hours worked: ");
+        hoursWorked = scanner.nextInt();
+        calculateSalary(rate, hoursWorked);
     }
-    public static void calculateSalary(int fee, int hours){
-        int commonWork = 40, overTime = hours - commonWork;
-        double salary = 0, overTimeSalary = 0;
-        if(hours > commonWork){
-            overTimeSalary = overTime * (fee * 1.5) ;
-            salary = overTimeSalary + (commonWork * fee);
+    public static void calculateSalary(int rate, int hoursWorked){
+        int commonWork = 40, overTime = hoursWorked - commonWork;
+        double salary = 0, overTimePay = 0;
+        if(hoursWorked > commonWork){
+            overTimePay = overTime * (rate * 1.5) ;
+            salary = overTimePay + (commonWork * rate);
         }
         else{
-            salary = hours * fee;
+            salary = hoursWorked * rate;
         }
-        System.out.println("Regular pay: " + (commonWork * fee));
-        System.out.println("Overtime pay: " + overTimeSalary);
+        System.out.println("Regular pay: " + (commonWork * rate));
+        System.out.println("Overtime pay: " + overTimePay);
         System.out.println("Total salary = " + salary);
     }
 }
